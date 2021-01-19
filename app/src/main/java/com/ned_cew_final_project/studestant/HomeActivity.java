2ebd811e0053977f10303c0c24b53ffc79a8f860
Sohaib-50 <i.am_sa@yahoo.com>
2021-01-19 21:59:43 +0500

    cleaned up mainactivity to make room for signin stuff

--- app/src/main/java/com/ned_cew_final_project/studestant/HomeActivity.java
+++ app/src/main/java/com/ned_cew_final_project/studestant/HomeActivity.java
@@ -1,14 +1,58 @@
 package com.ned_cew_final_project.studestant;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+    Button btn_todo_list;
+    Button btn_wikipedia_search;
+    Button btn_formulas_cheatsheet;
+    Button btn_pomodoro_timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+
+        btn_todo_list = findViewById(R.id.btn_todo_list);
+        btn_wikipedia_search = findViewById(R.id.btn_wikipedia_search);
+        btn_formulas_cheatsheet = findViewById(R.id.btn_formulas_cheatsheet);
+        btn_pomodoro_timer = findViewById(R.id.btn_pomodoro_timer);
+
+        btn_todo_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, TodoListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_wikipedia_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, WikipediaSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_formulas_cheatsheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, FormulasCheatsheetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_pomodoro_timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, PomodoroTimerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/ned_cew_final_project/studestant/MainActivity.java
+++ app/src/main/java/com/ned_cew_final_project/studestant/MainActivity.java
@@ -20,11 +20,6 @@ import com.google.firebase.firestore.auth.User;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG_SIGNIN = "Firebase Sign In";
-    Button btn_todo_list;
-    Button btn_wikipedia_search;
-    Button btn_formulas_cheatsheet;
-    Button btn_pomodoro_timer;
-
     private FirebaseAuth mAuth;  // firebase authentication instance
 
     @Override
@@ -33,47 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();  // get an instance of firebaseauth
-
-
-        btn_todo_list = findViewById(R.id.btn_todo_list);
-        btn_wikipedia_search = findViewById(R.id.btn_wikipedia_search);
-        btn_formulas_cheatsheet = findViewById(R.id.btn_formulas_cheatsheet);
-        btn_pomodoro_timer = findViewById(R.id.btn_pomodoro_timer);
-
-        btn_todo_list.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TodoListActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_wikipedia_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WikipediaSearchActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_formulas_cheatsheet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FormulasCheatsheetActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn_pomodoro_timer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PomodoroTimerActivity.class);
-                startActivity(intent);
-            }
-        });
     }
-}
-
 
     @Override
     protected void onStart() {
@@ -82,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
-
 
     private void createAccount(String email, String password)
     {
@@ -115,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signIn(String email, String password) {
-        Log.d(TAG, "signIn:" + email);
+        Log.d(TAG_SIGNIN, "signIn:" + email);
         if (!validateForm()) {
             return;
         }
