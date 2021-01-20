c353a9e3f797267f0e74676ebaa308d1b1b84b7c
Sohaib-50 <i.am_sa@yahoo.com>
2021-01-20 19:41:25 +0500

    moved homeactity java content from main to firebaseauthui branch

--- app/src/main/java/com/ned_cew_final_project/studestant/HomeActivity.java
+++ app/src/main/java/com/ned_cew_final_project/studestant/HomeActivity.java
@@ -1,27 +1,18 @@
 package com.ned_cew_final_project.studestant;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
     Button btn_todo_list;
     Button btn_wikipedia_search;
     Button btn_formulas_cheatsheet;
     Button btn_pomodoro_timer;
-//    Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +23,6 @@ public class HomeActivity extends AppCompatActivity {
         btn_wikipedia_search = findViewById(R.id.btn_wikipedia_search);
         btn_formulas_cheatsheet = findViewById(R.id.btn_formulas_cheatsheet);
         btn_pomodoro_timer = findViewById(R.id.btn_pomodoro_timer);
-//        btn_logout = findViewById(R.id.btn_logout_home);
 
         btn_todo_list.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,45 +55,5 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-//        btn_logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                logOut();
-//            }
-//        });
     }
-
-//
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        checkAuthState();
-//    }
-////
-////    @Override
-////    protected void onResume() {
-////        super.onResume();
-////        checkAuthState();
-////    }
-//
-//    private void checkAuthState() {
-//        if (FirebaseAuth.getInstance().getCurrentUser() == null)
-//        {
-//            Toast.makeText(this, "Logging out", Toast.LENGTH_SHORT).show();
-//            logOut();
-//        }
-//    }
-
-//    private void logOut() {
-//       AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
-//           @Override
-//           public void onComplete(@NonNull Task<Void> task) {
-//               if (task.isSuccessful())
-//               {
-//                   Toast.makeText(HomeActivity.this, "Logged out successfully", Toast.LENGTH_SHORT).show();
-//               }
-//           }
-//       });
-//    }
 }
