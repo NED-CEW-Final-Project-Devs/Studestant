826c57e4b2f9df48ffc272fe3612b4cf94f84ffb
RohanAhmed1 <rohanahmed212212@gmail.com>
2021-01-22 16:14:59 +0500

    first working of the wikipedia search

--- app/src/main/java/com/ned_cew_final_project/studestant/WikipediaSearchActivity.java
+++ app/src/main/java/com/ned_cew_final_project/studestant/WikipediaSearchActivity.java
@@ -2,7 +2,11 @@ package com.ned_cew_final_project.studestant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class WikipediaSearchActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class WikipediaSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wikipedia_search);
+    }
+
+
+    public void btnclick(View view) {
+
+        EditText et_wiki= (EditText)findViewById(R.id.editText_wiki);
+        String et1=et_wiki.getText().toString();
+
+        String url= "https://en.wikipedia.org/wiki/" + et1;
+
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
     }
 }
