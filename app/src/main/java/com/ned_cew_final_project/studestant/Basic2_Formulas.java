b03697c76520e314fcca5365b0f974e5b58d3dd3
moizali123 <moizalizirak@gmail.com>
2021-01-24 20:06:08 +0500

    Formulas Activity has been updated

--- app/src/main/java/com/ned_cew_final_project/studestant/Basic2_Formulas.java
+++ app/src/main/java/com/ned_cew_final_project/studestant/Basic2_Formulas.java
@@ -2,21 +2,18 @@ package com.ned_cew_final_project.studestant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Basic_Formulas extends AppCompatActivity {
+public class Basic2_Formulas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic__formulas);
+        setContentView(R.layout.activity_basic2__formulas);
     }
 
-    public void nex(View view) {
-        Intent intent = new Intent(this,Basic2_Formulas.class);
-        startActivity(intent);
-
+    public void pr(View view) {
+        super.finish();
     }
 }
